4b2768354d72fee08453523baef6ab31f7476136
mgreen1092 <morgan.green0209@gmail.com>
2022-10-23 16:42:32 -0400

    move to next question

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //select all elements
+//Main Page
 let mainPage = document.getElementById('main-page')
-// console.log(mainPage)
+
 //instructions modal
 let modal = document.querySelector('#modal')
 let openModal = document.querySelector('#openModal')
 let modalTextbox = document.querySelector('#modal-textbox')
 let closeModal = document.querySelector('#close')
 
+//Main page elements
 let nameInput = document.querySelector('.active')
 let playGameButton = document.querySelector('#play-game')
 let questionSlot = document.querySelector('h2')
@@ -49,68 +51,71 @@ let questions = [
 //Global Variables
 let questionsCorrect = 0
 playGameButton.addEventListener('click', questionsAppear)
+nextButton.addEventListener('click', questionsAppear)
 let randomQuestion = Math.floor(Math.random()*questions.length);
 
-
 function questionsAppear () {
-    //how to clear the background
-    // mainPage.classList.remove('active')
+    //remove answer modal when entering a new question
+    answerModal.style.display = 'none';
+    //remove background color change when entering a new question
+    let colorChange = document.querySelectorAll('.color-change')
+    colorChange.forEach(c => {
+        c.style.backgroundColor = 'white'
+        // console.log(c)
+    })
+    //removes main display to allow questions to appear
     mainPage.style.display = 'none';
-    for (let i=0; i<questions.length; i++) {
-        // let randomQuestion = Math.floor(Math.random()*questions.length);
-        // console.log(randomQuestion);
-        let questionsAppear = questions[randomQuestion].question;
-        // console.log(questionsAppear);
-        questionAnswerA = questions[randomQuestion].answers[0];
-        questionAnswerB = questions[randomQuestion].answers[1];
-        questionAnswerC = questions[randomQuestion].answers[2];
-        questionSlot.innerText = questionsAppear
-        answerSlot.style.display = 'block';
-        answer0.innerText = questionAnswerA;
-        answer1.innerText = questionAnswerB;
-        answer2.innerText = questionAnswerC;
-    }
-    
+    //re-call randomQuestion 
+    randomQuestion = Math.floor(Math.random()*questions.length);
+    //display questions
+    let questionsAppear = questions[randomQuestion].question;
+    questionSlot.innerText = questionsAppear
+    //display answer options
+    questionAnswerA = questions[randomQuestion].answers[0];
+    questionAnswerB = questions[randomQuestion].answers[1];
+    questionAnswerC = questions[randomQuestion].answers[2];
+    answerSlot.style.display = 'block';
+    answer0.innerText = questionAnswerA;
+    answer1.innerText = questionAnswerB;
+    answer2.innerText = questionAnswerC;
 }
 
-//if button selected matches the value in 'correct' background turns green and the next question appears
-//if correct, add one to questionsCorrect array
-
+//selecting answers
 answer0.addEventListener('click', answerSelection)
 answer1.addEventListener('click', answerSelection)
 answer2.addEventListener('click', answerSelection)
 
-
 function answerSelection (evt) {
-    console.log(evt.target.id[6])
+    //if button selected matches the value in 'correct' background turns green and response modal appears
+    // console.log(evt.target.id[6])
     console.log(questions[randomQuestion].correct)
     if (evt.target.id[6] == questions[randomQuestion].correct) {
         // console.log(evt.target.id[6])
+        //change correct answer background color to green
         evt.target.style.backgroundColor = 'lightgreen'
         setTimeout (function() {
+            //correct modal appears
             answerModal.style.display = 'block';
             correctModal.style.display = 'block';
+            //if correct, add one to questionsCorrect array
             questionsCorrect += 1;
             // console.log(questionsCorrect)
         }, 100)
     } else {
+        //if incorrect, background changes to lightcoral
         evt.target.style.backgroundColor = 'lightcoral'
         setTimeout (function() {
+            //incorrect modal appears
             answerModal.style.display = 'block';
             incorrectModal.style.display = 'block';
         }, 100)
     }
+    //remove the question that was asked from the array so the questions won't repeat
+    questions.splice(randomQuestion, 1)
+    // console.log(questions)
 }
 
+//if back button is selected, the main page appears
 
 
-
-//questions will have a class of hidden until they are ready to appear
-//Ex: the first question has a class of hidden, but when user hits play game, it will have a class of active
-
-//When a user gets a correct response the background of the correct answer will turn green
-
-//If incorrect, the background of the selected response will turn red and the correct response will turn green
-
-//Once question is marked as correct or incorrect, the page will refresh and new question will appear
 
