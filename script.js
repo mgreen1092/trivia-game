6b9b4acd5fd8d96267f04f6b91fcbd463a6063c4
mgreen1092 <morgan.green0209@gmail.com>
2022-10-23 15:59:28 -0400

    answer modals

--- script.js
+++ script.js
@@ -1,50 +1,109 @@
 //select all elements
 let mainPage = document.getElementById('main-page')
-console.log(mainPage)
+// console.log(mainPage)
+//instructions modal
+let modal = document.querySelector('#modal')
+let openModal = document.querySelector('#openModal')
+let modalTextbox = document.querySelector('#modal-textbox')
+let closeModal = document.querySelector('#close')
+
 let nameInput = document.querySelector('.active')
 let playGameButton = document.querySelector('#play-game')
 let questionSlot = document.querySelector('h2')
+
+//Answer Section
 let answerSlot = document.querySelector('#answer-section')
-let answerA = document.querySelector('#answerA')
-let answerB = document.querySelector('#answerB')
-let answerC = document.querySelector('#answerC')
+let answer0 = document.querySelector('#answer0')
+let answer1 = document.querySelector('#answer1')
+let answer2 = document.querySelector('#answer2')
+
+//elements of response modal
+let answerModal = document.querySelector('#response-modal')
+let correctModal = document.querySelector('#correct-answer')
+let incorrectModal = document.querySelector('#incorrect-answer')
+let nextButton = document.querySelector('#next-button')
+
+
+//Instructions open
+function openInstructions () {
+    modal.style.display = 'block';
+}
+//Instructions close 
+function closeInstructions () {
+    modal.style.display = 'none';
+}
+openModal.addEventListener("click", openInstructions)
+closeModal.addEventListener("click", closeInstructions)
 
 //click button to go to next page with questions
 let questions = [
     {question: 'What is my name?',
      answers: ['A: Morgan', 'B: Elizabeth', 'C: Green'],
-     correct: 'Morgan',
+     correct: 0,
     },
     {question: "Where do I live?",
      answers: ["A: Boston", "B: Acton", "C: Phoenix"],
-     correct: 'Acton',
+     correct: 1,
     }
 ]
-let questionsCorrect = []
+//Global Variables
+let questionsCorrect = 0
 playGameButton.addEventListener('click', questionsAppear)
-// console.log(questions[0].question)
+let randomQuestion = Math.floor(Math.random()*questions.length);
+
+
 function questionsAppear () {
     //how to clear the background
     // mainPage.classList.remove('active')
     mainPage.style.display = 'none';
     for (let i=0; i<questions.length; i++) {
-        let randomQuestion = Math.floor(Math.random()*questions.length);
-        console.log(randomQuestion);
+        // let randomQuestion = Math.floor(Math.random()*questions.length);
+        // console.log(randomQuestion);
         let questionsAppear = questions[randomQuestion].question;
-        console.log(questionsAppear);
+        // console.log(questionsAppear);
         questionAnswerA = questions[randomQuestion].answers[0];
         questionAnswerB = questions[randomQuestion].answers[1];
         questionAnswerC = questions[randomQuestion].answers[2];
         questionSlot.innerText = questionsAppear
         answerSlot.style.display = 'block';
-        answerA.innerText = questionAnswerA;
-        answerB.innerText = questionAnswerB;
-        answerC.innerText = questionAnswerC;
+        answer0.innerText = questionAnswerA;
+        answer1.innerText = questionAnswerB;
+        answer2.innerText = questionAnswerC;
     }
     
 }
 
-//question appears when screen clears with three multiple choice answers
+//if button selected matches the value in 'correct' background turns green and the next question appears
+//if correct, add one to questionsCorrect array
+
+answer0.addEventListener('click', answerSelection)
+answer1.addEventListener('click', answerSelection)
+answer2.addEventListener('click', answerSelection)
+
+
+function answerSelection (evt) {
+    console.log(evt.target.id[6])
+    console.log(questions[randomQuestion].correct)
+    if (evt.target.id[6] == questions[randomQuestion].correct) {
+        // console.log(evt.target.id[6])
+        evt.target.style.backgroundColor = 'lightgreen'
+        setTimeout (function() {
+            answerModal.style.display = 'block';
+            correctModal.style.display = 'block';
+            questionsCorrect += 1;
+            // console.log(questionsCorrect)
+        }, 100)
+    } else {
+        evt.target.style.backgroundColor = 'lightcoral'
+        setTimeout (function() {
+            answerModal.style.display = 'block';
+            incorrectModal.style.display = 'block';
+        }, 100)
+    }
+}
+
+
+
 
 //questions will have a class of hidden until they are ready to appear
 //Ex: the first question has a class of hidden, but when user hits play game, it will have a class of active
